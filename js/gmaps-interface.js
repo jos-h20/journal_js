5c5b381c5de0d76510603520538b1667c187506e
Jason Awbrey and Josh Overly <jason@>
2016-03-14 16:26:22 -0700

    Add GMaps

--- js/gmaps-interface.js
+++ js/gmaps-interface.js
@@ -1,18 +1,3 @@
-var Journal = require('./../js/journal.js').Journal;
-var moment = require('moment');
-
-$(document).ready(function(){
-  $('#journal').submit(function(event){
-    event.preventDefault();
-    var title = $('#title').val();
-    var body = $('#body').val();
-    var entry = new Journal(title, body);
-    var count = entry.countWords();
-    $('#word-count').text(count);
-    $('#date').text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    });
-  });
-
 "use strict";
 (function(root, factory) {
   if(typeof exports === 'object') {
@@ -2283,19 +2268,3 @@ if (!Array.prototype.indexOf) {
 
 return GMaps;
 }));
-
-$(document).ready(function(){
-  $('#time').text(moment());
-});
-
-var apiKey = "adfb6c08f301ba43007da5fb6b0c50b9";
-
-$(document).ready(function(){
-  $('#weatherLocation').click(function(){
-    var city = $('#location').val();
-    $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-    $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
-  });
-});
