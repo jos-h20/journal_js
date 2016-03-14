c543d24b0fa3ba2db5485db92fc7e1363faa92c1
Jason Awbrey and Josh Overly <jason+josh@>
2016-03-14 09:25:58 -0700

    Add require statements and prompt node package for cli to work correctly

--- js/cli.js
+++ js/cli.js
@@ -1,3 +1,7 @@
+var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
+
 prompt.get('goal', function(err, result){
   var output = pingPong(result.goal);
   output.forEach(function(element){
